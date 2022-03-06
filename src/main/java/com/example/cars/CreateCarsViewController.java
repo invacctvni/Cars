a9cf18a1abe3661aaa9aab84b66931648a2efacd
Jiaqi Chen <200477892@student.georgianc.on.ca>
2022-03-06 14:15:57 -0500

    create-car-fxml complete

--- src/main/java/com/example/cars/CreateCarsViewController.java
+++ src/main/java/com/example/cars/CreateCarsViewController.java
@@ -1,4 +1,27 @@
 package com.example.cars;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class CreateCarsViewController {
+
+    @FXML
+    private ComboBox<?> brandComboBox;
+
+    @FXML
+    private Label msgLabel;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Spinner<?> priceSpinner;
+
+    @FXML
+    private CheckBox sportCheckBox;
+
 }
